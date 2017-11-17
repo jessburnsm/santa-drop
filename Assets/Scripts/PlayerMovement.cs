14f35dbb6cb1f3096996eae0596930353f323146
Jessica Burns <jessburnsm@gmail.com>
2017-11-16 23:48:23 -0500

    Adjust player's movement
    Still adjusting movement speed and ability

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,12 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public float speed;             //Floating point variable to store the player's movement speed.
+    // Player's movement speed
+	public float speed = 10f;
+
+    // Player's max movement speed
+    public float maxSpeed = 10.5f;
+
 	// public Animator animator;
 	private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
 	// private SpriteRenderer sprite;  //Store a reference to the sprite renderer
@@ -47,6 +52,10 @@ public class PlayerMovement : MonoBehaviour
         // Set the player's up/down movement
         // Clamp the player's movement so that they can't stop or go in the opposite direction
         Vector2 movement = new Vector2(0, Mathf.Clamp(moveVertical, -.5f, .5f));
+        //Vector2 movement = new Vector2(0, moveVertical);
         rb2d.AddForce(movement * speed);
+
+        // Clamp the force on the player to prevent them from falling too fast
+        rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxSpeed);
     }
 }
