52925ac1aeb3effdd0984df5ad5481d6f1175f97
Jessica Burns <jessburnsm@gmail.com>
2017-11-16 23:22:24 -0500

    Add camera behavior script
    Adds a camera script to follow the player's movement in the game.

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+	void LateUpdate () {
+        Vector3 newPos = new Vector3(0f, target.position.y, -10f);
+        transform.position = newPos;
+	}
+}
