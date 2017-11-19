07fa1a6460dc78b092c38c93f4b1756a9b8234fa
Jessica Burns <jessburnsm@gmail.com>
2017-11-18 22:20:59 -0500

    Refactor player movement
    Also fixes some minor bugs with movement velocity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,59 +3,90 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Player's movement speed
+    // Player's left/right movement speed
 	public float speed = 10f;
 
-    // Player's max movement speed
+    // Player's max falling speed
     public float maxSpeed = 10.5f;
 
-	// public Animator animator;
-	private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
-	// private SpriteRenderer sprite;  //Store a reference to the sprite renderer
+    // Jump variables
+    public float jumpCooldown = 5f;
+    public float jumpForce = 5f;
+    private bool hasJumped;
+    private float jumpTime = 0f;
 
-	// Use this for initialization
-	void Start()
+    // Rigidbody2D component required to use 2D Physics.
+    private Rigidbody2D rb2d;
+
+    // private Animator animator;
+    // private SpriteRenderer sprite;
+
+    // Use this for initialization
+    void Start()
 	{
-		//Get and store a reference to the Rigidbody2D + SpriteRenderer component so that we can access it.
 		rb2d = GetComponent<Rigidbody2D>();
-		// sprite = GetComponent<SpriteRenderer>();
-	}
+        // sprite = GetComponent<SpriteRenderer>();
+        // animator = GetComponent<Animator>();
+    }
 
-	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
-	void FixedUpdate()
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
 	{
 		//Store the current horizontal input in the float moveHorizontal.
 		float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
 
-        //If the player is moving, flip the sprite so that the character is facing the right direction
-        //		if(moveHorizontal < 0)
-        //		{
-        //			animator.SetBool("isWalking", true);
-        //			sprite.flipX = true;
-        //		}
-        //		else if(moveHorizontal > 0)
-        //		{
-        //			animator.SetBool("isWalking", true);
-        //			sprite.flipX = false;
-        //		}
-        //		else
-        //		{
-        //			animator.SetBool("isWalking", false);
-        //		}
+        // Prevent jitter when player is at the edge of the playing field
+        if ( (moveHorizontal > 0 && transform.position.x >= 5f) || (moveHorizontal < 0 && transform.position.x <= -5f) )
+            moveHorizontal = 0;
 
         // Set the player's left/right movement
         Vector2 velocity = rb2d.velocity;
         velocity.x = moveHorizontal * speed;
         rb2d.velocity = velocity;
 
-        // Set the player's up/down movement
-        // Clamp the player's movement so that they can't stop or go in the opposite direction
-        Vector2 movement = new Vector2(0, Mathf.Clamp(moveVertical, -.5f, .5f));
-        //Vector2 movement = new Vector2(0, moveVertical);
-        rb2d.AddForce(movement * speed);
+        // Check if player is eligible for jumping
+        hasJumped = jumpCooldownActive();
+
+        // Check if player is trying to jump
+        if (Input.GetKeyDown("space") && !hasJumped)
+        {
+            rb2d.AddForce(new Vector2(0, -rb2d.velocity.y * jumpForce));
+            hasJumped = true;
+            jumpTime = Time.time;
+        }            
 
         // Clamp the force on the player to prevent them from falling too fast
         rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxSpeed);
+
+        // Ensure player is staying in playing field
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -5f, 5f), transform.position.y);
+
+        // updateAnimations(float moveHorizontal, Animator animator, SpriteRenderer sprite)
+
+    }
+
+    private bool jumpCooldownActive()
+    {
+        // Determine if enough time has passed to end cooldown
+        return !((Time.time - jumpTime) >= jumpCooldown);
+    }
+
+    private void updateAnimations(float moveHorizontal, Animator animator, SpriteRenderer sprite)
+    {
+        //If the player is moving, flip the sprite so that the character is facing the right direction
+        if(moveHorizontal < 0)
+        {
+        	animator.SetBool("isWalking", true);
+        	sprite.flipX = true;
+        }
+        else if(moveHorizontal > 0)
+        {
+        	animator.SetBool("isWalking", true);
+        	sprite.flipX = false;
+        }
+        else
+        {
+        	animator.SetBool("isWalking", false);
+        }
     }
 }
