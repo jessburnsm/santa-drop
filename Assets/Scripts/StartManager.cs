edd9f2255b1663f9827c823273a36877ce2d2e98
Jessica Burns <jessburnsm@gmail.com>
2017-11-28 23:58:23 -0500

    Update game menus
    Add restart button to end game screen + add main menu screen

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour {
+	public Animator animator;
+
+	public void OpenInstructions(){
+		animator.SetBool("isOpen", true);
+	}
+
+	public void CloseInstructions(){
+		animator.SetBool("isOpen", false);
+	}
+
+	public void StartGame(){
+		SceneManager.LoadScene("Scene01");
+	}
+}
