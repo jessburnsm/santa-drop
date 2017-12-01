054c9359dcf8af2c08bf1bf0f85ab18afd340020
Jessica Burns <jessburnsm@gmail.com>
2017-11-30 22:37:51 -0500

    Create level end menu

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,11 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
+	// Level end menu
+	public Animator levelEndMenu;
+	public Text presentText;
+	public Text chimneyText;
+	public Text finalText;
+
 	// Score counter
 	private int score = 0;
 	private int bonus = 10;
-	private bool bonusApplied = false;
 	public Text scoreText;
+
+	// Level Generator
+	public LevelGenerator levelGenerator;
 
     // Game lives
     public int lives = 3;
@@ -26,12 +34,6 @@ public class LevelManager : MonoBehaviour {
 		scoreText.text = "Score: " + score;
 	}
 
-	public void addBonus(){
-		score += bonus;
-		bonusApplied = true;
-		updateScoreText();
-	}
-
 	public void updateScore(){
 		score += 1;
 		updateScoreText();
@@ -42,9 +44,21 @@ public class LevelManager : MonoBehaviour {
         SceneManager.LoadScene("GameOver");
 	}
 
-	public void endLevel(bool bonus = false){
-		Debug.Log("Level has ended!");
-		SceneManager.LoadScene("Main");
+	public void endLevel(bool bonusApplied = false){
+		presentText.text = "Presents: " + score + "/" + levelGenerator.numberOfCollectibles;
+
+		int finalScore = score;
+		if (bonusApplied) {
+			finalScore += bonus;
+			chimneyText.text = "Chimney Bonus: " + bonus + "/" + bonus;
+		} else {
+			chimneyText.text = "Chimney Bonus: 0/" + bonus;
+		}
+
+		finalText.text = "Final Score: " + finalScore;
+
+
+		levelEndMenu.SetBool("isOpen", true);
 	}
 
     public void hitObstacle(){
@@ -53,4 +67,13 @@ public class LevelManager : MonoBehaviour {
         if (lives <= 0)
             endGame();
     }
+
+	public void restartGame(){
+		SceneManager.LoadScene("Main");
+	}
+
+	public void restartLevel(){
+		int scene = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene (scene, LoadSceneMode.Single);
+	}
 }
